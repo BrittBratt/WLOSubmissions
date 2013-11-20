cf5716d1650326ecf5ce060e1d6c2a3104484949
Brittany Isabelle <brittbrat9693@gmail.com>
2013-11-20 12:35:33 -0500

    Added more fields

--- Story Submission Form.php
+++ Story Submission Form.php
@@ -1,7 +1,14 @@
-<form>
-<strong>Name:</strong> <input type="text" name="name"><br>
-<strong>Email:</strong> <input type="text" name="email">
-<strong>Is this original:</strong>
-<input type="radio" name="original" value="yes">yes<br>
-<input type="radio" name="original" value="no">No
+<form xmlns="http://www.w3.org/1999/xhtml">
+<strong>Name:</strong> <input name="name" type="text" /><br /><br />
+<strong>Email:</strong> <input name="email" type="text" /><br /><br />
+<strong>Is this original:</strong><br /><br />
+<input name="original" type="radio" value="yes" />yes<br />
+<input name="original" type="radio" value="no" />No<br /><br />
+<strong>Source:</strong> <input name="source" type="text" /><br /><br />
+<strong>Does this have Mature Content</strong><br /><br />
+<input name="content" type="radio" value="yes" />yes<br />
+<input name="content" type="radio" value="no" />No<br /><br />
+<strong>Story Title:</strong> <input name="storytitle" type="text" /><br /><br />
+<center><strong>PLEASE NOTE ARE WORKING ON THE CUSTOM FORM AND AT THIS POINT I CAN'T IMPLEMENT A MULTIPLE TEXT AREA THAT WILL SUBMIT. PLEASE GIVE US TIME TO DO THIS SO FOR NOW PLEASE SUBMIT ALL STORIES VIA A GOOGLE DOC AND MAKE SURE ITS ACCESSIBLE TO THE PUBLIC. I HOPE TO FIX THIS ERROR SOON.<center></strong>
+<strong>Story Link:</strong> <input name="storytitle" type="text" /><br /><br />
 </form>
